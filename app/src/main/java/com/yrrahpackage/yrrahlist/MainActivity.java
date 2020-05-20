68c9e5591911c95c62ae3c401904cb84016f4275
Yrrah <harry.dfh@gmail.com>
2020-05-20 11:12:46 +0200

    Added my first commit. This is basically just for some repetition. I got rusty.

--- app/src/main/java/com/yrrahpackage/yrrahlist/MainActivity.java
+++ app/src/main/java/com/yrrahpackage/yrrahlist/MainActivity.java
@@ -3,6 +3,8 @@ package com.yrrahpackage.yrrahlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPressed(View view){
+        Toast.makeText(getApplicationContext(),"Button is pressed!", Toast.LENGTH_SHORT).show();
     }
 }
